273438d1a75c425db204650b1b7ae13139f0241f
Chirag <chiraggoyel98@gmail.com>
2022-07-08 00:39:24 +0530

    minor changes

--- login-signup-form/src/components/home/home.js
+++ login-signup-form/src/components/home/home.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from "react"
 import "./home.css"
 
@@ -11,18 +10,4 @@ const Home = ({setLoginUser}) => {
     )
 }
 
-=======
-import React from "react"
-import "./home.css"
-
-const Home = ({setLoginUser}) => {
-    return (
-       <div className="homepage">
-            <h1>Home Page</h1>
-            <div className="button" onClick={() => setLoginUser({})}>Logout</div> 
-       </div> 
-    )
-}
-
->>>>>>> 299e4fe436b952f1cbc8058c4c7f0ecbd5fec514
 export default Home
--- login-signup-form/src/components/login/login.js
+++ login-signup-form/src/components/login/login.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, {useState} from "react"
 import "./login.css"
 import axios from "axios"
@@ -48,55 +47,4 @@ const Login = ({ setLoginUser}) => {
     )
 }
 
-=======
-import React, {useState} from "react"
-import "./login.css"
-import axios from "axios"
-import { useNavigate } from "react-router-dom"
-
-const Login = ({ setLoginUser}) => {
-
-    const navigate = useNavigate()
-
-    const [user, setUser] = useState({
-        email: "",
-        password: "" 
-    })
-
-    const updateChange = e => {
-        const {name, value} = e.target
-        setUser({
-            ...user,
-            [name] : value
-        })
-    }
-
-    const login = () => {
-        axios.post("http://localhost:9002/login", user)
-        
-        .then(res => {
-            alert(res.data.message)
-
-            setLoginUser(res.data.user)
-
-            navigate("/")
-        })
-    }
-
-    return (
-       <div className="loginpage"> 
-            <h1>Login</h1>
-            <div className="msg"></div>
-
-            <input type="text" placeholder="Email id" name="email" value={user.email} onChange={updateChange}></input>
-            <input type="password" placeholder="Password" name="password" value={user.password} onChange={updateChange}></input>
-            
-            <div className="button" onClick={login}>Login</div>
-            <div className="msg">Didn't have an account</div>
-            <div className="button" onClick={() => navigate("/register")}>Sign Up</div>
-       </div> 
-    )
-}
-
->>>>>>> 299e4fe436b952f1cbc8058c4c7f0ecbd5fec514
 export default Login
